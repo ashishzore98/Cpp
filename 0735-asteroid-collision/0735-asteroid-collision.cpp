1ff53574bf9d16397c86649a6441f9521ad7abdf
Ashish Zore <zoreashish98@gmail.com>
2026-07-23 21:04:08 +0530

    Time: 0 ms (100%), Space: 22.3 MB (19.49%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for (int a : asteroids) {
+            bool d = false;
+            while (!st.empty() && a < 0 && st.top() > 0) {
+                if (st.top() < -a) {
+                    st.pop();
+                }
+                else if (st.top() == -a) {
+                    st.pop();
+                    d = true;
+                    break;
+                }
+                else {
+                    d = true;
+                    break;
+                }
+            }
+            if (!d) {
+                st.push(a);
+            }
+        }
+        vector<int> ans(st.size());
+        for (int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
